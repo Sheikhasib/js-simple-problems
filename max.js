600f4e3d8c5628e04ff3cb7e87529b1e15b94585
Sheikhasib <sheikhhasib037@gmail.com>
2021-08-07 18:12:50 +0600

    max done

---
+++ max.js
@@ -0,0 +1,39 @@
+const business = 1350;
+const minister = 250;
+const army = 600;
+// if(business>minister){
+//     console.log('Business is bigger');
+// }
+// else{
+//     console.log('minister is bigger');
+// }
+
+// Task 1:create a function that takes three numbers as input parameter and returns you the largest number of the three
+
+// Task 2:writ a function find the smallest of three numbers
+
+function findLargest(first,second){
+    if(first>second){
+        return first;
+    }
+    else{
+        return second;
+    }
+}
+
+const largest = findLargest(356,764);
+console.log('largest is:',largest);
+
+// Compare three
+// if(business > minister && business > army){
+//     console.log('Business is bigger');
+// }
+// else if(minister > business && minister > army){
+//     console.log('Minister is bigger');
+// }
+// else {
+//     console.log('army is bigger');
+// }
+
+var max = Math.max(business,minister,army);
+// console.log('largest is max',max);
