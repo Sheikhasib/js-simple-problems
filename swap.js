df22d535782802e04e6dbb02bbccdb868e90e5bd
Sheikhasib <sheikhhasib037@gmail.com>
2021-08-07 15:10:01 +0600

    swap done

---
+++ swap.js
@@ -0,0 +1,12 @@
+var first = 5;
+var second = 7;
+console.log(first,second);
+// First Approach
+// var temp = first;
+// first = second;
+// second = temp;
+// console.log(first,second);
+
+// Destructuring
+[first,second] = [second,first];
+console.log(first,second);
